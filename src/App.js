af7af894cdf0621832a4f1b2b5e9d4165fbfb73a
Gabriel Sarubo <gabrielsarubo@hotmail.com>
2019-11-09 20:56:20 -0300

    Add Ninja component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import Ninjas from './Ninjas'
 import './App.css';
 
 class App extends React.Component {
+  state = {
+    // ninjas is an array of objects (AND a property of the object "state") with multiple properties
+    ninjas : [
+      { name: "Ryu", age: "31", belt: "black", id: 1 },
+      { name: "Gabs", age: "21", belt: "white", id: 2 },
+      { name: "Luis Felipe", age: "15", belt: "red", id: 3 }
+    ]
+  }
+  
   render() {
     return (
       <div className="App">
         <h1>Hello world!</h1>
+
+        {/* Passing ninjas (an array inside the state of this class) as a prop */}
+        <Ninjas ninjas={this.state.ninjas} />
       </div>
     );
   }
